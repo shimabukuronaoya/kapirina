8823b830b5a59b3b14648ac663ecf453c182c1d2
Naoya-0126 <115777716+Naoya-0126@users.noreply.github.com>
2024-02-24 20:16:08 +0900

    ブログページ　sp

--- blog/index.php
+++ blog/index.php
@@ -16,7 +16,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 <section class="l-section blogArea">
         <div class="l-content">
           <ul class="blogNavList">
-            <li class="blogNavList__item"><a href="" class="blogNavList__link">すべて</a></li>
+            <li class="blogNavList__item"><a href="" class="blogNavList__link is-active">すべて</a></li>
             <li class="blogNavList__item"><a href="" class="blogNavList__link">お知らせ</a></li>
             <li class="blogNavList__item"><a href="" class="blogNavList__link">ファンダイビング</a></li>
             <li class="blogNavList__item"><a href="" class="blogNavList__link">体験ダイビング</a></li>
@@ -26,15 +26,17 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
           <div class="blogBox">
             <ul class="blogList">
               <li class="blogList__item">
-                <div class="blogList__imgBox">
-                  <img src="/assets/img/dummy/blog-arc01.jpg" alt="">
-                </div>
-                <div class="blogList__category">
-                  <p class="blogList__tag">ファンダイビング</p>
-                  <p class="blogList__date">2023/12/30</p>
-                </div>
-                <p class="blogList__ttl">2023年潜り納め</p>
-                <p class="blogList__text">ついにこの日がやって参りましたぁ～２０２３年潜り納めです。しかも天気がいい☀☀☀そして三銃士は今回３大ポイント制覇!!!…</p>
+                <a class="blogList__link" href="">
+                    <div class="blogList__imgBox">
+                        <img src="/assets/img/dummy/blog-arc01.jpg" alt="">
+                      </div>
+                      <div class="blogList__category">
+                        <p class="blogList__tag">ファンダイビング</p>
+                        <p class="blogList__date">2023/12/30</p>
+                      </div>
+                      <p class="blogList__ttl">2023年潜り納め</p>
+                      <p class="blogList__text">ついにこの日がやって参りましたぁ～２０２３年潜り納めです。しかも天気がいい☀☀☀そして三銃士は今回３大ポイント制覇!!!…</p>
+                </a>
               </li>
               <li class="blogList__item">
                 <div class="blogList__imgBox">
@@ -60,7 +62,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
               </li>
               <li class="blogList__item">
                 <div class="blogList__imgBox">
-                  <img src="/assets/img/dummy/blog-arc03.jpg" alt="">
+                  <img src="/assets/img/dummy/blog-arc04.jpg" alt="">
                 </div>
                 <div class="blogList__category">
                   <p class="blogList__tag">ファンダイビング</p>
@@ -69,6 +71,103 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
                 <p class="blogList__ttl">念願の太陽がきたー！！！！</p>
                 <p class="blogList__text">待ちに待った太陽が出てくれて、今日は暖かく過ごしやすい気温でした(*´ω｀*)これからクリスマスモードから一気に年末モードに…</p>
               </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc05.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                  <p class="blogList__tag">ファンダイビング</p>
+                  <p class="blogList__date">2023/12/25</p>
+                </div>
+                <p class="blogList__ttl">🌲メリークリスマス🌲</p>
+                <p class="blogList__text">皆さん。🌲メリークリスマス🌲クリスマスだし、プレゼントがてらマンタでも出ないか期待したのですがマンタのマの字も感…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc06.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                  <p class="blogList__tag">ファンダイビング</p>
+                  <p class="blogList__date">2023/12/24</p>
+                </div>
+                <p class="blogList__ttl">クリスマスイブ🎄</p>
+                <p class="blogList__text">今朝から雨が降っていました午後から風が強く吹き、時化る予報だったので、本日は午前２本で終了！気温が低いので、水中…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc07.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                  <p class="blogList__tag">ファンダイビング</p>
+                  <p class="blogList__date">2023/12/23</p>
+                </div>
+                <p class="blogList__ttl">((((；ﾟДﾟ))))ｶﾞｸｶﾞｸﾌﾞﾙﾌﾞﾙダイビング</p>
+                <p class="blogList__text">約１週間ぶりのダイビングです！ここ数日の間に宮古島にも寒波がやってきまして、気温が一週間前と比べて、一気に１０℃近く…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc08.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                  <p class="blogList__tag">ファンダイビング</p>
+                  <p class="blogList__date">2023/12/15</p>
+                </div>
+                <p class="blogList__ttl">ギンギラギン☀夏日のような暑さι(´Д｀υ)ｱﾂｨｰ</p>
+                <p class="blogList__text">今日は夏日のような暑さι(´Д｀υ)ｱﾂｨｰ 伊良部のポイントをまわりました！L字アーチにいつもいるロウニンアジは残念なが…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc09.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                  <p class="blogList__tag">ファンダイビング</p>
+                  <p class="blogList__date">2023/12/15</p>
+                </div>
+                <p class="blogList__ttl">4年ぶりにご夫婦揃っての来店</p>
+                <p class="blogList__text">４年ぶりにご夫婦揃っての来店!!!!!!!!!元気なお姿を見れてなんだか嬉しいです４年ぶりであたふたするのかと思いきや、そんな雰…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc10.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                    <p class="blogList__tag">ファンダイビング</p>
+                    <p class="blogList__date">2023/12/10</p>
+                  </div>
+                  <p class="blogList__ttl">いただきマンタ！のはずが、、、（笑）(´Д｀υ)ｱﾂｨｰ</p>
+                  <p class="blogList__text">あっち～！１２月とは思えないほどの気持ちのいい気温今日は久しぶりに伊良部へ！やっぱり白鳥幼稚園は楽しいな～流れが…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc11.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                    <p class="blogList__tag">ファンダイビング</p>
+                    <p class="blogList__date">2023/12/06</p>
+                  </div>
+                  <p class="blogList__ttl">持ってるゲストさん！</p>
+                  <p class="blogList__text">昨日の宣言通りに、まわりをきょろきょろ見ながら、マンタを探しましたが今日は会えませんでした(;´∀｀)マンタが出そうな…</p>
+              </li>
+              <li class="blogList__item">
+                <div class="blogList__imgBox">
+                  <img src="/assets/img/dummy/blog-arc12.jpg" alt="">
+                </div>
+                <div class="blogList__category">
+                  <p class="blogList__tag">ファンダイビング</p>
+                  <p class="blogList__date">2023/12/05</p>
+                </div>
+                <p class="blogList__ttl">マンタ現る！？</p>
+                <p class="blogList__text">タイトルの通り、今日はなんとマンタに会いました！船から上がった後に、ゲストさんが『いや～ここのポイントってマンタ…</p>
+              </li>
+            </ul>
+          </div>
+          <div class="blogPageNationBox">
+            <ul class="blogPageNationList">
+                <li class="blogPageNationList__item"><a class="blogPageNationList__link is-active" href="">1</a></li>
+                <li class="blogPageNationList__item"><a class="blogPageNationList__link" href="">2</a></li>
+                <li class="blogPageNationList__item"><a class="blogPageNationList__link" href="">3</a></li>
+                <li class="blogPageNationList__item"><a class="blogPageNationList__link" href="">4</a></li>
+                <li class="blogPageNationList__item"><a class="blogPageNationList__link" href="">5</a></li>
             </ul>
           </div>
         </div>
