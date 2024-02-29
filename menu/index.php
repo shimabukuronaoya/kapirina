b35eea312f6a80b8e1db2efb6bb5c711a8c84e64
Naoya-0126 <115777716+Naoya-0126@users.noreply.github.com>
2024-02-29 22:28:41 +0900

    メニューページ・FAQページ

--- menu/index.php
+++ menu/index.php
@@ -16,188 +16,133 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 <!-- menu -->
 <section class="menuArea l-pd">
   <div class="l-content">
-
-    <section class="menuSec">
-      <div class="menuDiving__imgFrame js-parallax">
-        <img src="/assets/img/menu/img_fun-main.jpg" alt="">
-      </div>
-      <h2 class="c-ttl01 left">
-        <span class="c-ttl01__ja">Fun Diving</span>
-        <span class="c-ttl01__en">ファンダイビング</span>
-      </h2>
-      <div class="menuDiving__contBox">
-        <div class="menuDiving__txtBox">
-          <p class="menuDiving__lead">
-            宮古島ダイビングの楽しみ方のバラエティーは数知れず…<br class="pc">皆様のダイビングスタイルやリクエストに<br class="pc">合わせてご案内させて頂きます。
-          </p>
-          <a href="/menu/fundiving/" class="c-btn menuDiving__link">詳しくはこちら</a>
-        </div>
-        <div class="menuPrice__box">
-          <p class="menuPrice__ttl">ボートファンダイビング</p>
-          <ul class="menuPriceList">
-            <li class="menuPriceList__item">
-              <p class="menuPriceList__name"><span class="num">1</span>ダイブ</p>
-              <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">13,200</span>/名(税込)</p>
-            </li>
-            <li class="menuPriceList__item">
-              <p class="menuPriceList__name"><span class="num">2</span>ダイブ</p>
-              <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">17,600</span>/名(税込)</p>
-            </li>
-            <li class="menuPriceList__item">
-              <p class="menuPriceList__name"><span class="num">3</span>ダイブ</p>
-              <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">22,000</span>/名(税込)</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
-    <section class="menuSec">
-      <div class="menuDiving__imgFrame js-parallax">
-        <img src="/assets/img/menu/img_trial-main.jpg" alt="">
-      </div>
-      <h2 class="c-ttl01 left">
-        <span class="c-ttl01__ja">Trial Diving</span>
-        <span class="c-ttl01__en">体験ダイビング</span>
-      </h2>
-      <div class="menuDiving__contBox">
-        <div class="menuDiving__txtBox">
-          <p class="menuDiving__lead">
-            お気軽に水中の世界を楽しみたい方にピッタリ!!!!<br>
-            初めての方でも安心で楽しんでいただけるように<br>
-            サポートします。
-          </p>
-          <a href="/menu/trialdiving/" class="c-btn menuDiving__link">詳しくはこちら</a>
-        </div>
-        <div class="menuPrice__box">
-          <p class="menuPrice__ttl">ボート体験ダイビング</p>
-          <ul class="menuPriceList">
-            <li class="menuPriceList__item">
-              <p class="menuPriceList__name"><span class="num">1</span>ダイブ</p>
-              <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">16,500</span>/名(税込)</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
-    <section class="menuSec">
-      <div class="menuDiving__imgFrame js-parallax">
-        <img src="/assets/img/menu/img_license-main.jpg" alt="">
-      </div>
-      <h2 class="c-ttl01 left">
-        <span class="c-ttl01__ja">NAUI License Course</span>
-        <span class="c-ttl01__en">NAUIライセンス講習</span>
-      </h2>
-      <div class="menuDiving__contBox">
-        <div class="menuDiving__txtBox">
-          <p class="menuDiving__lead">
-            ダイビングの必要な知識とスキルを身につけて<br>
-            水中世界を楽しんでみませんか？
-          </p>
-          <a href="/menu/owd/" class="c-btn menuDiving__link">オープンウォーター講習</a>
-          <a href="/menu/aowd/" class="c-btn menuDiving__link">アドバンス講習</a>
-        </div>
-        <div class="menuPrice__box">
-          <p class="menuPrice__ttl">NAUIライセンス講習</p>
-          <ul class="menuPriceList">
-            <li class="menuPriceList__item">
-              <p class="menuPriceList__name">オープンウォーターコース</p>
-              <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">74,800</span>/名(税込)</p>
-            </li>
-            <li class="menuPriceList__item">
-              <p class="menuPriceList__name">アドバンスコース</p>
-              <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">44,000</span>/名(税込)</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
-    <section class="menuSec rental">
-      <h2 class="c-ttl01 left">
-        <span class="c-ttl01__ja">Rental Equipment</span>
-        <span class="c-ttl01__en">レンタル器材</span>
-      </h2>
-
-      <ul class="menuPriceList row">
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">フルレンタル</p>
-          <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">4,400</span>/名(税込)</p>
+    <div class="menuSection">
+      <ul class="menuList">
+        <li class="menuList__item menuFun">
+          <div class="menuList__imgBox">
+            <img src="/assets/img/menu/img_fun-main.jpg" alt="">
+          </div>
+          <h2 class="c-ttl01">
+            <span class="c-ttl01__ja">Fun Diving</span>
+            <span class="c-ttl01__en">ファンダイビング</span>
+          </h2>
+          <div class="menuList__contBox">
+            <div class="menuList__txtBox">
+              <p class="menuList__lead">宮古島ダイビングの楽しみ方のバラエティーは数知れず…<br class="pc">皆様のダイビングスタイルやリクエストに<br class="pc">合わせてご案内させて頂きます。</p>
+              <a href="/menu/fundiving" class="c-btn menuList__link">詳しくはこちら</a>
+            </div>
+            <div class="menuList__courseBox">
+              <p class="courseBox__ttl">ボートファンダイビング</p>
+              <ul class="courseList">
+                <li class="courseList__item">
+                  <p class="courseName">1ダイブ</p>
+                  <p class="coursePrice"><span class="yen">¥</span>13,200<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="courseList__item">
+                  <p class="courseName">2ダイブ</p>
+                  <p class="coursePrice"><span class="yen">¥</span>17,600<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="courseList__item">
+                  <p class="courseName">3ダイブ</p>
+                  <p class="coursePrice"><span class="yen">¥</span>22,000<span class="tax">/名(税込)</span></p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </li>
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">BC</p>
-          <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">1,100</span>/名(税込)</p>
+        <li class="menuList__item menuTrial">
+          <div class="menuList__imgBox">
+            <img src="/assets/img/menu/img_trial-main.jpg" alt="">
+          </div>
+          <h2 class="c-ttl01">
+            <span class="c-ttl01__ja">Trial Diving</span>
+            <span class="c-ttl01__en">体験ダイビング</span>
+          </h2>
+          <div class="menuList__contBox">
+            <div class="menuList__txtBox">
+              <p class="menuList__lead">お気軽に水中の世界を楽しみたい方にピッタリ!!!! 初めての方でも安心で楽しんでいただけるように サポートします。</p>
+              <a href="/menu/trialdiving/" class="c-btn menuList__link">詳しくはこちら</a>
+            </div>
+            <div class="menuList__courseBox">
+              <p class="courseBox__ttl">ボート体験ダイビング</p>
+              <ul class="courseList">
+                <li class="courseList__item">
+                  <p class="courseName">1ダイブ</p>
+                  <p class="coursePrice"><span class="yen">¥</span>16,500<span class="tax">/名(税込)</span></p>
+                </li>
+                </ul>
+            </div>
+          </div>
         </li>
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">レギュレータ</p>
-          <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">1,100</span>/名(税込)</p>
+        <li class="menuList__item menuLicense">
+          <div class="menuList__imgBox">
+            <img src="/assets/img/menu/img_license-main.jpg" alt="">
+          </div>
+          <h2 class="c-ttl01">
+            <span class="c-ttl01__ja">NAUI License Course</span>
+            <span class="c-ttl01__en">NAUIライセンス講習</span>
+          </h2>
+          <div class="menuList__contBox">
+            <div class="menuList__txtBox">
+              <p class="menuList__lead">ダイビングの必要な知識とスキルを身につけて<br class="pc">水中世界を楽しんでみませんか？</p>
+              <a href="/menu/owd/" class="c-btn menuList__link">オープンウォーター講習</a>
+              <a href="/menu/aowd/" class="c-btn menuList__link">アドバンス講習</a>
+            </div>
+            <div class="menuList__courseBox">
+              <p class="courseBox__ttl">NAUIライセンス講習</p>
+              <ul class="courseList">
+                <li class="courseList__item">
+                  <p class="courseName">オープンウォーターコース</p>
+                  <p class="coursePrice"><span class="yen">¥</span>74,800<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="courseList__item">
+                  <p class="courseName">アドバンスコース</p>
+                  <p class="coursePrice"><span class="yen">¥</span>44,000<span class="tax">/名(税込)</span></p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </li>
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">
-            軽器材
-            <span class="subTxt">(マスク/シュノーケル/ブーツ/フィン)</span>
-          </p>
-          <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">1,650</span>/名(税込)</p>
-        </li>
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">ウェットスーツ</p>
-          <p class="menuPriceList__price"><span class="yen">¥</span><span class="num">1,100</span>/名(税込)</p>
-        </li>
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">防水デジタルカメラ</p>
-          <p class="menuPriceList__price">無料</p>
-        </li>
-        <li class="menuPriceList__item">
-          <p class="menuPriceList__name">水中ライト</p>
-          <p class="menuPriceList__price">無料</p>
+        <li class="menuList__item menuRental">
+          <h2 class="c-ttl01">
+            <span class="c-ttl01__ja">Rental Equipment</span>
+            <span class="c-ttl01__en">レンタル器材</span>
+          </h2>
+          <div class="menuRentalBox">
+              <ul class="menuRentalList">
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">フルレンタル</p>
+                  <p class="menuRentalList__price"><span class="yen">¥</span>4,400<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">BC</p>
+                  <p class="menuRentalList__price"><span class="yen">¥</span>1,100<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">レギュレータ</p>
+                  <p class="menuRentalList__price"><span class="yen">¥</span>1,100<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">軽器材<br><span class="detail">(マスク/シュノーケル/ブーツ/フィン)</span></p>
+                  <p class="menuRentalList__price"><span class="yen">¥</span>1,650<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">ウェットスーツ</p>
+                  <p class="menuRentalList__price"><span class="yen">¥</span>1,100<span class="tax">/名(税込)</span></p>
+                </li>
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">防水デジタルカメラ</p>
+                  <p class="menuRentalList__price">無料</p>
+                </li>
+                <li class="menuRentalList__item">
+                  <p class="menuRentalList__name">水中ライト</p>
+                  <p class="menuRentalList__price">無料</p>
+                </li>
+              </ul>
+            </div>
         </li>
       </ul>
-
-    </section>
-
-    <section class="menuSec reserve">
-      <h2 class="c-ttl01 left">
-        <span class="c-ttl01__ja">Reserve Calendar</span>
-        <span class="c-ttl01__en">予約カレンダー</span>
-      </h2>
-      <div class="menuDiving__contBox">
-        <div class="menuReserve__txtBox">
-          <p class="menuReserve__lead">
-            参加人数と参加メニューを選択すると空き状況が出ます。予約日を選択して「予約フォーム」へお進みください。
-          </p>
-        </div>
-        <div class="menuReserve__calender">
-          <div class="okabanCalendar" style="opacity: 0;"></div>
-          <script>
-          var shop_id = 'kapilina';
-          </script>
-          <script src="https://app.okaban.work/common/js/calendar.js"></script>
-        </div>
-      </div>
-    </section>
-
-    <section class="menuSec safety">
-      <h2 class="c-ttl01 left">
-        <span class="c-ttl01__ja">Safety Guideline</span>
-        <span class="c-ttl01__en">注意事項</span>
-      </h2>
-
-      <ul class="menuSafetyList">
-        <li class="menuSafetyList__item">
-          <h3 class="menuSafetyList__ttl">キャンセル料について</h3>
-          <p class="menuSafetyList__txt c-txtDot">前日までのキャンセルには料金は発生しません。但し、当日のキャンセルはお一人様につき5,000円のキャンセル料金が発生しますのでご了承ください。</p>
-          <p class="menuSafetyList__txt c-txtDot">当日の天候・海況状況・台風・飛行機の欠航などの際は、キャンセル料は発生しません。</p>
-        </li>
-        <li class="menuSafetyList__item">
-          <h3 class="menuSafetyList__ttl">宮古島美ら海協力金について</h3>
-          <p class="menuSafetyList__txt c-txtDot">宮古島市周辺の海域でダイビングをお楽しみいただく皆様へ、1日につき¥500のご協力をお願いしております。美しい宮古島の海を長く保つため、ご協力をお願いいたします。(上記料金には含まれていません。)</p>
-          <a href="https://m-chura.com/churaumi.html" target="_blank" class="c-btn window menuSafetyList__link">美ら海協力金ついて</a>
-        </li>
-      </ul>
-
-    </section>
-
+    </div>
   </div>
 </section>
 <!-- /menu -->
