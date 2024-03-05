53a7baafc8a745f6ca388d688f0c3521c15a7990
Naoya-0126 <115777716+Naoya-0126@users.noreply.github.com>
2024-03-05 23:56:16 +0900

    体験ダイビング　

--- menu/trialdiving/index.php
+++ menu/trialdiving/index.php
@@ -17,27 +17,32 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 <section class="l-section divingFirstArea">
   <div class="l-content">
 
-    <p class="divingFirst__lead">ダイビングには興味があるけど…と思っている貴方！<br class="pc">宮古島の綺麗な海で潜ってみませんか？ 水中世界には今までにない感動が待っています。</p>
+    <p class="divingFirst__lead">
+    ダイビングには興味があるけど…と思っている貴方！<br class="pc">宮古島の綺麗な海で潜ってみませんか？ 水中世界には今までにない感動が待っています。
+    </p>
 
     <ul class="divingAboutList">
+
       <li class="divingAboutList__item">
-        <div class="divingAboutList__imgBox js-parallax"><img src="/assets/img/menu/trialdiving/img_first01.jpg" alt=""></div>
+        <div class="divingAboutList__imgBox">
+          <img src="/assets/img/menu/trialdiving/img_first01.jpg" alt="">
+        </div>
         <div class="divingAboutList__contBox">
-          <h2 class="divingAboutList__ttl">少人数で安心・安全</h2>
-          <p class="divingAboutList__txt">
-            ガイド1人に対してお客様2名までの完全少人数制でのご案内になります。ガイドがきちんと不安を解消して水中へお連れしますので、初めての方でも安心してお楽しみ頂けます。
-          </p>
+          <p class="divingAboutList__ttl">少人数で安心・安全</p>
+          <p class="divingAboutList__txt">ガイド1人に対してお客様2名までの完全少人数制でのご案内になります。ガイドがきちんと不安を解消して水中へお連れしますので、初めての方でも安心してお楽しみ頂けます。</p>
         </div>
       </li>
+
       <li class="divingAboutList__item">
-        <div class="divingAboutList__imgBox js-parallax"><img src="/assets/img/menu/trialdiving/img_first02.jpg" alt=""></div>
+        <div class="divingAboutList__imgBox">
+          <img src="/assets/img/menu/trialdiving/img_first02.jpg" alt="">
+        </div>
         <div class="divingAboutList__contBox">
-          <h2 class="divingAboutList__ttl">宮古島ならではの<br>あざやかな水中世界</h2>
-          <p class="divingAboutList__txt">
-            宮古島ならではのサンゴやカラフルな魚達…時にはウミガメにも会えるかも…！
-          </p>
+          <p class="divingAboutList__ttl">宮古島ならではの<br>あざやかな水中世界</p>
+          <p class="divingAboutList__txt">宮古島ならではのサンゴやカラフルな魚達…時にはウミガメにも会えるかも…！</p>
         </div>
       </li>
+
     </ul>
 
     <ul class="c-firstPhotoList">
@@ -45,6 +50,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
       <li class="c-firstPhotoList__item js-parallax"><img src="/assets/img/menu/trialdiving/img_first-photo03.jpg" alt="" class="c-firstPhotoList__img"></li>
       <li class="c-firstPhotoList__item js-parallax"><img src="/assets/img/menu/trialdiving/img_first-photo02.jpg" alt="" class="c-firstPhotoList__img"></li>
     </ul>
+    
   </div>
 </section>
 <!-- /divingFirst -->
@@ -53,40 +59,59 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 <!-- flow -->
 <section class="l-section flowArea">
   <div class="l-content">
-
     <div class="c-contentBox l-pd">
       <h2 class="c-ttl01 center">
         <span class="c-ttl01__ja">体験ダイビングの流れ</span>
         <span class="c-ttl01__en">Flow</span>
       </h2>
 
-      <ol class="flowList">
+      <ul class="flowList">
         <li class="flowList__item">
           <div class="flowList__contBox">
-            <h3 class="flowList__ttl">ブリーフィング＆レクチャー</h3>
-            <p class="flowList__txt">マスクの付け方や水中での呼吸の仕方、ダイビング器材の使い方などをレクチャーします。</p>
+            <p class="flowList__ttl">ブリーフィング＆レクチャー</p>
+            <p class="flowList__txt">
+            マスクの付け方や水中での呼吸の仕方、ダイビング器材の使い方などをレクチャーします。
+            </p>
           </div>
-          <div class="flowList__imgBox"><img src="/assets/img/menu/trialdiving/img_flow01-main.jpg" alt=""></div>
-          <div class="flowList__miniImgBox"><img src="/assets/img/menu/trialdiving/img_flow01-sub.jpg" alt=""></div>
+          <div class="flowList__imgBox">
+            <img src="/assets/img/menu/trialdiving/img_flow01-main.jpg" alt="">
+          </div>
+          <div class="flowList__subImgBox">
+            <img src="/assets/img/menu/trialdiving/img_flow01-sub.jpg" alt="">
+          </div>
         </li>
         <li class="flowList__item">
           <div class="flowList__contBox">
-            <h3 class="flowList__ttl">まずはシュノーケリング</h3>
-            <p class="flowList__txt">ダイビングの前に、まずはシュノーケルでしっかりと呼吸の練習をします。透明度の高い宮古島の海は、海面からのぞきこむだけでも美しいサンゴと色とりどりの魚たちを見ることができます。</p>
+            <p class="flowList__ttl">まずはシュノーケリング</p>
+            <p class="flowList__txt">
+            ダイビングの前に、まずはシュノーケルでしっかりと呼吸の練習をします。透明度の高い宮古島の海は、海面からのぞきこむだけでも美しいサンゴと色とりどりの魚たちを見ることができます。
+            </p>
           </div>
-          <div class="flowList__imgBox"><img src="/assets/img/menu/trialdiving/img_flow02-main.jpg" alt=""></div>
-          <div class="flowList__miniImgBox"><img src="/assets/img/menu/trialdiving/img_flow02-sub.jpg" alt=""></div>
+          <div class="flowList__imgBox">
+            <img src="/assets/img/menu/trialdiving/img_flow02-main.jpg" alt="">
+          </div>
+          <div class="flowList__subImgBox">
+            <img src="/assets/img/menu/trialdiving/img_flow02-sub.jpg" alt="">
+          </div>
         </li>
         <li class="flowList__item">
           <div class="flowList__contBox">
-            <h3 class="flowList__ttl">ダイビング開始！</h3>
-            <p class="flowList__txt">水に慣れてきたら水中世界へ！<br>
-              お客様2名につきガイドスタッフが1名付くので安心してお楽しみください。水中でもボードを使って宮古島の海の見所を紹介します！</p>
+            <p class="flowList__ttl">ダイビング開始！</p>
+            <p class="flowList__txt">
+            水に慣れてきたら水中世界へ！<br>お客様2名につきガイドスタッフが1名付くので安心してお楽しみください。<br>水中でもボードを使って宮古島の海の見所を紹介します！
+            </p>
           </div>
-          <div class="flowList__imgBox"><img src="/assets/img/menu/trialdiving/img_flow03-main.jpg" alt=""></div>
-          <div class="flowList__miniImgBox"><img src="/assets/img/menu/trialdiving/img_flow03-sub.jpg" alt=""></div>
+          <div class="flowList__imgBox">
+            <img src="/assets/img/menu/trialdiving/img_flow03-main.jpg" alt="">
+          </div>
+          <div class="flowList__subImgBox">
+            <img src="/assets/img/menu/trialdiving/img_flow03-sub.jpg" alt="">
+          </div>
         </li>
-      </ol>
+      </ul>
+
+      
+    
     </div>
   </div>
 </section>
@@ -101,53 +126,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
       <span class="c-ttl01__en">Schedule</span>
     </h2>
 
-    <div class="scheduleRowBox">
-      <ul class="scheduleList">
-        <li class="scheduleList__item">
-          <div class="scheduleList__txtBox">
-            <p class="scheduleList__time">8:00</p>
-            <p class="scheduleList__txt">宿泊先にお迎え</p>
-          </div>
-        </li>
-        <li class="scheduleList__item">
-          <div class="scheduleList__txtBox">
-            <p class="scheduleList__time">8:30</p>
-            <p class="scheduleList__txt">出港</p>
-          </div>
-        </li>
-        <li class="scheduleList__item">
-          <div class="scheduleList__txtBox">
-            <p class="scheduleList__time">9:00</p>
-            <p class="scheduleList__txt">
-              シュノーケル＆体験ダイビング<br>
-              (追加ダイビングも含む)
-            </p>
-          </div>
-        </li>
-        <li class="scheduleList__item">
-          <div class="scheduleList__txtBox">
-            <p class="scheduleList__time">11:30</p>
-            <p class="scheduleList__txt">ランチ</p>
-          </div>
-        </li>
-        <li class="scheduleList__item">
-          <div class="scheduleList__txtBox">
-            <p class="scheduleList__time">12:30</p>
-            <p class="scheduleList__txt">シュノーケル＆体験ダイビング</p>
-          </div>
-        </li>
-        <li class="scheduleList__item">
-          <div class="scheduleList__txtBox">
-            <p class="scheduleList__time">14:00</p>
-            <p class="scheduleList__txt">帰港</p>
-          </div>
-        </li>
-      </ul>
-    </div>
-
-    <div class="schedule__noteBox">
-      <p class="schedule__note">※上記のタイムスケジュールは目安になります。変更もありますのでご了承ください</p>
-    </div>
+    
   </div>
 </section>
 <!-- /schedule -->
@@ -155,99 +134,105 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 <!-- menuAbout -->
 <section class="l-section menuAboutArea">
   <div class="l-content --s">
-    <h2 class="c-ttl01">
-      <span class="c-ttl01__ja">メニュー概要</span>
+    <div class="c-contentBox l-pd">
+      <h2 class="c-ttl01">
+        <span class="c-ttl01__ja">メニュー概要</span>
       <span class="c-ttl01__en">Menu About</span>
-    </h2>
+      </h2>
 
-    <ul class="menuAboutList">
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">ご利用料金について</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="c-txtDot">1ダイブ　<span class="price">¥<span class="num">16,500</span>/名(税込)</span></p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">料金に含まれるもの</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="txt">ダイビング器材すべて、タンク代、ガイド料、乗船料、ドリンク、消費税</p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">料金に含まれないもの</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="c-txtDot">美ら海協力金<span class="price">¥<span class="num">500</span></span></p>
-              <p class="c-txtDot">追加ダイビング<span class="price">¥<span class="num">4,400</span>(税込)</span></p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">参加資格</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="txt">10歳以上(未成年の方は保護者の同意が必要です)</p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">所要時間</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="txt">約7時間</p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">必要なもの</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="txt">タオル、水着</p>
-              <a href="/shop/faq/#faq-belongings" class="txt c-linkTxt arrow">ダイビング時の服装や持ち物は？｜よくある質問</a>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">お支払方法</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="txt">現金またはクレジットカード</p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-      <li class="menuAboutList__item">
-        <dl class="menuAboutContList">
-          <div class="menuAboutContList__item">
-            <dt class="menuAboutContList__ttl">注意事項</dt>
-            <dd class="menuAboutContList__cont">
-              <p class="c-txtDot">当日のコンディションにより安全面を考慮した上でツアー変更・中止になる場合があります。ご理解宜しくお願い致します。</p>
-              <p class="c-txtDot">船酔いが心配な方は『酔い止め』の服用をおすすめします。</p>
-              <p class="c-txtDot">飛行機搭乗日のダイビングツアーはおこなっておりません。</p>
-            </dd>
-          </div>
-        </dl>
-      </li>
-    </ul>
-
-    <a href="https://app.okaban.work/form/?shop_id=kapilina&category_id=experience" target="_blank" class="c-btn center window menuAbout__btmLink">体験ダイビングを予約する</a>
+      <ul class="menuAboutList">
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">ご利用料金について</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="menuAboutContentList__txt">1ダイブ
+                <span class="yen">¥</span>
+                <span class="num">16,500/名(税込)</span></p></dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">料金に含まれるもの</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="menuAboutContentList__txt">ダイビング器材すべて、タンク代、ガイド料、乗船料、ドリンク、消費税</p>
+              </dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">料金に含まれないもの</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="c-txtDot">美ら海協力金
+                <span class="yen">¥</span>
+                <span class="num">500</span></p>
+                <p class="c-txtDot">追加ダイビング
+                <span class="yen">¥</span>
+                <span class="num">4,400</span></p></dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">参加資格</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="menuAboutContentList__txt">10歳以上(未成年の方は保護者の同意が必要です)</p>
+              </dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">所要時間</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="menuAboutContentList__txt">約7時間</p>
+              </dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">必要なもの</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="menuAboutContentList__txt">タオル、水着</p>
+                <p class="menuAboutContentList__txt"><a href="/shop/faq/#faq-belongings" class="menuAboutContentList__link">ダイビング時の服装や持ち物は？｜よくある質問</a></p>
+                
+                
+              </dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">お支払方法</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="menuAboutContentList__txt">現金またはクレジットカード</p>
+              </dd>
+            </div>
+          </dl>
+        </li>
+        <li class="menuAboutList__item">
+          <dl class="menuAboutContentList">
+            <div class="menuAboutContentList__item">
+              <dt class="menuAboutContentList__ttl">注意事項</dt>
+              <dd class="menuAboutContentList__txtBox">
+                <p class="c-txtDot">当日のコンディションにより安全面を考慮した上でツアー変更・中止になる場合があります。ご理解宜しくお願い致します。</p>
+                <p class="c-txtDot">船酔いが心配な方は『酔い止め』の服用をおすすめします。</p>
+                <p class="c-txtDot">飛行機搭乗日のダイビングツアーはおこなっておりません。</p>
+              </dd>
+            </div>
+          </dl>
+        </li>
+      </ul>
+    </div>  
   </div>
 </section>
 <!-- /menuAbout -->
