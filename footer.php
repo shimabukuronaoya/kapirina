ace69e194cf1b3c2f6a44012b1e997515bbf0d6d
Naoya-0126 <115777716+Naoya-0126@users.noreply.github.com>
2024-03-18 07:19:10 +0900

    fotter sp 　ボタン

--- footer.php
+++ footer.php
@@ -104,36 +104,9 @@
 
   <div class="footerBitknot"><a href="https://bitknot.co.jp/" target="_blank"><img src="/assets/img/common/logo_bitknot.svg" alt="" class="footerBitknot__img"></a></div>
 
-  <!-- <div class="footerInfo l-pd">
-    <div class="l-content">
-      <div class="footerInfo__main">
-        <a href="/" class="footerInfoLogo"><img src="/assets/img/common/logo_white.png" alt="宮古島ダイビングショップ カピリナ Kapilina" class="footerInfoLogo__img"></a>
-        <ul class="footerInfoSnsList">
-          <li class="footerInfoSnsList__item insta"><a href="https://www.instagram.com/kapilina_miyakojima_diving/" target="_blank" class="footerInfoSnsList__link"><img src="/assets/img/common/ico_sns-instagram-white.svg" alt="instagram" class="footerInfoSnsList__img"></a></li>
-          <li class="footerInfoSnsList__item fb"><a href="https://ja-jp.facebook.com/miyakojima.kapilina/" target="_blank" class="footerInfoSnsList__link"><img src="/assets/img/common/ico_sns-facebook-white.svg" alt="facebook" class="footerInfoSnsList__img"></a></li>
-          <li class="footerInfoSnsList__item line"><a href="https://page.line.me/953lutzq?openQrModal=true" target="_blank" class="footerInfoSnsList__link"><img src="/assets/img/common/ico_sns-line-white.svg" alt="line" class="footerInfoSnsList__img"></a></li>
-        </ul>
-        <ul class="footerInfoList">
-          <li class="footerInfoList__item address">〒906-0008 沖縄県宮古島市平良字荷川取104-1-D号室</li>
-          <li class="footerInfoList__item tel"><a href="tel:08092433505">080-9243-3505</a></li>
-        </ul>
-
-        <a href="#" class="pageTop"><img src="/assets/img/common/ico_pagetop.svg" alt="" class="pageTop__icon"></a>
-      </div>
-
-      <div class="footerInfoBtm">
-        <ul class="footerBtmNavList">
-          <li class="footerBtmNavList__item"><a href="/contact/" class="footerBtmNavList__link">お問い合わせ</a></li>
-          <li class="footerBtmNavList__item"><a href="https://app.okaban.work/web/privacy/?shop_id=kapilina" target="_blank" class="footerBtmNavList__link">個人情報保護方針</a></li>
-          <li class="footerBtmNavList__item"><a href="/shop/#shopAbout" class="footerBtmNavList__link">ショップ概要</a></li>
-        </ul>
-        <small class="footerBtm__copy">&copy;Kapilina</small>
-      </div>
-    </div>
+  <div class="testArea">
+  <a href="#" class="button">予約専用フォーム</a>
   </div>
-
-  <div class="footerBitknot"><a href="https://bitknot.co.jp/" target="_blank"><img src="/assets/img/common/logo_bitknot.svg" alt="Powerd by BITKNOT ビットノット株式会社" class="footerBitknot__img"></a></div>
-</footer> -->
 
 
 </div> <!-- /l-mainContainer -->
